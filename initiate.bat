78c2e7b57e080a494442373053e252f0f002407a
arpit15mishra <arpit15mishra@gmail.com>
2019-07-10 23:41:12 +0530

    initiated the repository with the required files

---
+++ initiate.bat
@@ -0,0 +1,9 @@
+git init
+git add .
+git checkout initiate.bat
+git commit -m "initiated the repository with the required files"
+notepad readme.md
+git add readme.md
+git commit -m "added readme file to the repository"
+echo "git is initiated successfully"
+PAUSE
